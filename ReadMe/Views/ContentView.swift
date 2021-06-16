4dabf6970e3f037efcb2aa51e114c9dea2630af8
bogdanov <iceposhta@gmail.com>
2021-06-17 00:13:16 +0200

    show list of books in a content view

---
+++ ReadMe/Views/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  ReadMe
+//
+//  Created by bogdanov on 16.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(Library().sortedBooks, id: \.title) { book in
+                BookRow(book: book)
+            }.navigationBarTitle("My Library")
+        }
+    }
+}
+
+struct BookRow: View {
+    let book: Book
+
+    var body: some View {
+        NavigationLink(
+            destination: DetailView(book: book),
+            label: {
+                HStack {
+                    Book.Image(title: book.title)
+                    VStack(alignment: .leading) {
+                        Text(book.title)
+                            .font(.title2)
+                        Text(book.author)
+                            .font(.title3)
+                            .foregroundColor(.secondary)
+                    }
+                }.lineLimit(1)
+            })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
