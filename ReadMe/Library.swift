4c54f081550ef74cf0fafd06325a5ffcc54767fb
bogdanov <iceposhta@gmail.com>
2021-06-16 17:28:38 +0200

    add a Library mock

---
+++ ReadMe/Library.swift
@@ -0,0 +1,26 @@
+//
+//  Library.swift
+//  ReadMe
+//
+//  Created by bogdanov on 16.06.21.
+//
+
+import Foundation
+
+final class Library {
+    var sortedBooks: [Book] {booksCache}
+    
+    private var booksCache: [Book] = [
+        .init(title: "War and Peace", author: "Leo Tolstoy"),
+        .init(title: "Anna Karenina", author: "Leo Tolstoy"),
+        .init(title: "Crime and Punishment", author: "Fyodor Dostoevsky"),
+        .init(title: "The Master and Margarita", author: "Mikhail Bulgakov"),
+        .init(title: "Life and Fate", author: "Vasily Grossman"),
+        .init(title: "The Brothers Karamazov and Magic Sword", author: "Fyodor Dostoevsky"),
+        .init(title: "Dead Souls", author: "Nikolai Gogol"),
+        .init(title: "Eugene Onegin", author: "Aleksandr Pushkin"),
+        .init(title: "Lolita", author: "Vladimir Nabokov"),
+        .init(title: "Doctor Zhivago", author: "Boris Pasternak")
+
+    ]
+}
