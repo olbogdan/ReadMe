226c0bc887cec587a4de01cd171181d3de2a891e
bogdanov <iceposhta@gmail.com>
2021-06-17 00:12:05 +0200

    implemented: BookViews

---
+++ ReadMe/Views/BookViews.swift
@@ -0,0 +1,46 @@
+//
+//  BookViews.swift
+//  ReadMe
+//
+//  Created by bogdanov on 16.06.21.
+//
+
+import SwiftUI
+
+extension Book {
+    struct Image: View {
+        let title: String
+
+        var body: some View {
+            let symbol = SwiftUI.Image(title: title) ?? .init(systemName: "book")
+            symbol
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .font(.title.weight(.light))
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct Book_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Book.Image(title: Book().title)
+            Book.Image(title: "")
+            Book.Image(title: "🎪")
+        }
+    }
+}
+
+extension Image {
+    init?(title: String) {
+        guard let character = title.first,
+              case let symbolName = "\(character.lowercased()).square",
+              UIImage(systemName: symbolName) != nil
+        else {
+            return nil
+        }
+        self.init(systemName: symbolName)
+    }
+}
