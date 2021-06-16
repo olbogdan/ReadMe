94f60224f8ad847cd60c5cc4c5a27f8023f323ec
bogdanov <iceposhta@gmail.com>
2021-06-16 15:38:39 +0200

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by bogdanov on 16.06.21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
