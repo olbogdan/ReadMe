2ed2d144ad68f6f9ea25f93b498d6d5c6df221b4
bogdanov <iceposhta@gmail.com>
2021-06-20 18:15:09 +0200

    reformat code

--- ReadMe/Views/ContentView.swift
+++ ReadMe/Views/ContentView.swift
@@ -17,14 +17,14 @@ struct ContentView: View {
                 Button(action: {
                     addingNewBook = true
                 }) {
-                        Spacer()
-                        VStack(spacing: 6) {
-                            Image(systemName: "book.circle")
-                                .font(.system(size: 60))
-                            Text("Add New Book")
-                                .font(.title2)
-                        }
-                        Spacer()
+                    Spacer()
+                    VStack(spacing: 6) {
+                        Image(systemName: "book.circle")
+                            .font(.system(size: 60))
+                        Text("Add New Book")
+                            .font(.title2)
+                    }
+                    Spacer()
                 }
                 .buttonStyle(BorderlessButtonStyle())
                 .padding(.vertical, 8)
@@ -46,23 +46,23 @@ private struct BookRow: View {
     var body: some View {
         NavigationLink(
             destination: DetailView(book: book)) {
-                HStack {
-                    Book.Image(uiImage: library.uiImages[book], title: book.title, size: 80, cornerRadius: 12)
-                    VStack(alignment: .leading) {
-                        TitleAndAuthorStack(book: book, titleFont: .title2, authorFont: .title3)
-                        if !book.microReview.isEmpty {
-                            Spacer()
-                            Text(book.microReview)
-                                .font(.subheadline)
-                                .foregroundColor(.secondary)
-                        }
+            HStack {
+                Book.Image(uiImage: library.uiImages[book], title: book.title, size: 80, cornerRadius: 12)
+                VStack(alignment: .leading) {
+                    TitleAndAuthorStack(book: book, titleFont: .title2, authorFont: .title3)
+                    if !book.microReview.isEmpty {
+                        Spacer()
+                        Text(book.microReview)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
 
-                    }.lineLimit(1)
-                    Spacer()
-                    BookmarkButton(book: book)
-                        .buttonStyle(BorderlessButtonStyle())
-                }
-                .padding(.vertical, 8)
+                }.lineLimit(1)
+                Spacer()
+                BookmarkButton(book: book)
+                    .buttonStyle(BorderlessButtonStyle())
+            }
+            .padding(.vertical, 8)
         }
     }
 }
@@ -82,20 +82,22 @@ private struct SectionView: View {
 
     var body: some View {
         if let books = library.manuallySortedBooks[section] {
-            SwiftUI.Section(header: ZStack {
-                Image("BookTexture")
-                    .resizable()
-                    .scaledToFit()
-                    .listRowInsets(.init())
-                Text(title)
-                    .font(.custom("American Typewriter", size: 24))
-            }) {
-                    ForEach(books) {
-                        BookRow(book: $0)
-                    }
-                    .onDelete { indexSet in
-                        library.deleteBooks(atOffsets: indexSet, section: section)
-                    }
+            SwiftUI.Section(
+                header:
+                ZStack {
+                    Image("BookTexture")
+                        .resizable()
+                        .scaledToFit()
+                    Text(title)
+                        .font(.custom("American Typewriter", size: 24))
+                }.listRowInsets(.init())
+            ) {
+                ForEach(books) {
+                    BookRow(book: $0)
+                }
+                .onDelete { indexSet in
+                    library.deleteBooks(atOffsets: indexSet, section: section)
+                }
             }
         }
     }
