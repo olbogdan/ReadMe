fe25b14d993fb4f07dbbadb3ab6dcd2692747ee6
bogdanov <iceposhta@gmail.com>
2021-06-20 14:20:59 +0200

    introduce enum Section & ManuallySortedBooks Dictionary

--- ReadMe/Library.swift
+++ ReadMe/Library.swift
@@ -8,14 +8,26 @@
 import Combine
 import class UIKit.UIImage
 
-class Library: ObservableObject {
+enum Section {
+    case readMe
+    case finished
+}
+
+final class Library: ObservableObject {
     var sortedBooks: [Book] { booksCache }
-    
+
+    var manuallySortedBooks: [Section: [Book]] {
+        Dictionary(grouping: booksCache, by: \.readMe)
+            .mapKeys(Section.init)
+    }
+
     /// Add a new book at the start of the library's manually-sorted books.
     func addNewBook(_ book: Book, image: UIImage?) {
         booksCache.insert(book, at: 0)
         uiImages[book] = image
     }
+
+    @Published var uiImages: [Book: UIImage] = [:]
 
     @Published private var booksCache: [Book] = [
         .init(title: "War and Peace", author: "Leo Tolstoy", microReview: "describes oak on 5 pages"),
@@ -29,6 +41,23 @@ class Library: ObservableObject {
         .init(title: "Lolita", author: "Vladimir Nabokov"),
         .init(title: "Doctor Zhivago", author: "Boris Pasternak")
     ]
+}
 
-    @Published var uiImages: [Book: UIImage] = [:]
+// MARK: - private
+
+private extension Section {
+    init(readMe: Bool) {
+        self = readMe ? .readMe : .finished
+    }
+}
+
+private extension Dictionary {
+    /// Same values, corresponding to `map`ped keys.
+    ///
+    /// - Parameter transform: Accepts each key of the dictionary as its parameter
+    ///   and returns a key for the new dictionary.
+    /// - Postcondition: The collection of transformed keys must not contain duplicates.
+    func mapKeys<Transformed>(_ transform: (Key) throws -> Transformed) rethrows -> [Transformed: Value] {
+        .init(uniqueKeysWithValues: try map { (try transform($0.key), $0.value) })
+    }
 }
